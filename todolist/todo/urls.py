12097888bf877d358fd4262c5d6cecce3f205f6a
Rodrigo <rodrigocorrea2@hotmail.com>
2019-09-27 14:14:02 -0500

    complete feature
    when you click the todo it's now completed, also is red now

--- todolist/todo/urls.py
+++ todolist/todo/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
-	path('add', views.addTodo, name='add')
+	path('add', views.addTodo, name='add'),
+	path('complete/<todo_id>', views.completeTodo, name='complete')
 ]

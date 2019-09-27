fedada8302222ce4dcd9327c6ef76fae7e629cea
Rodrigo <rodrigocorrea2@hotmail.com>
2019-09-27 14:03:49 -0500

    make the sumit button work

--- todolist/todo/views.py
+++ todolist/todo/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
 from .models import Todo
 from .forms import TodoForm
 
@@ -7,3 +8,9 @@ def index(request):
 	form = TodoForm()
 	context = {'todo_list' : todo_list, 'form' : form}
 	return render(request, 'todo/index.html', context)
+
+@require_POST
+def addTodo(request):
+	form = TodoForm(request.POST)
+	print(request.POST['text'])
+	return redirect('index')

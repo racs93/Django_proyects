78e32f3321d6dec1309cbdc3d2c9f9b927f49dbb
Rodrigo <rodrigocorrea2@hotmail.com>
2019-09-27 13:54:13 -0500

    stylizing the button

--- todolist/todo/views.py
+++ todolist/todo/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Todo
+from .forms import TodoForm
 
 def index(request):
 	todo_list = Todo.objects.order_by('id')
-	context = {'todo_list' : todo_list}
+	form = TodoForm()
+	context = {'todo_list' : todo_list, 'form' : form}
 	return render(request, 'todo/index.html', context)

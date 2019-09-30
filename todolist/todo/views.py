cda4e8539c6e3400653fdcdfeecf09a5f5d8154c
Rodrigo <rodrigocorrea2@hotmail.com>
2019-09-29 22:28:48 -0500

    modifiyng the field
    using forms and modifyng the field

--- todolist/todo/views.py
+++ todolist/todo/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
 from .models import Todo
-from .forms import TodoForm
+from .forms import TodoForm, NewTodoForm
 
 def index(request):
 	todo_list = Todo.objects.order_by('id')
-	form = TodoForm()
-	context = {'todo_list' : todo_list, 'form' : form}
+	# form = TodoForm()
+	newtodoform = NewTodoForm()
+	context = {'todo_list' : todo_list, 'form' : newtodoform}
 	return render(request, 'todo/index.html', context)
 
 @require_POST
 def addTodo(request):
-	form = TodoForm(request.POST)
-	if form.is_valid():
-		new_todo = Todo(text=request.POST['text'])
-		new_todo.save()
+	# form = TodoForm(request.POST)
+	newtodoform = NewTodoForm(request.POST)
+	if newtodoform.is_valid():
+		# new_todo = Todo(text=form.cleaned_data['text'])
+		# new_todo.save()
+		new_todo = newtodoform.save()
 	return redirect('index')
 
 def completeTodo(request, todo_id):

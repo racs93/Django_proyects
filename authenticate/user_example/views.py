e486422895c0165a61c4de6d8d756482344726b1
Rodrigo <rodrigocorrea2@hotmail.com>
2019-09-30 00:26:43 -0500

    registration page
    using creation form to create the registration page.

--- authenticate/user_example/views.py
+++ authenticate/user_example/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 def index(request):
 	return render(request, 'user_example/index.html')
+
+def register(request):
+	form = UserCreationForm()
+	context = {'form' : form}
+	return render(request, 'registration/register.html', context)

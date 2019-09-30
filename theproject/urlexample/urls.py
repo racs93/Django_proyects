2e13d2c23ecf9e64b821958b8c4936574cd3237a
Rodrigo <rodrigocorrea2@hotmail.com>
2019-09-30 14:52:03 -0500

    new feature profile by default

--- theproject/urlexample/urls.py
+++ theproject/urlexample/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	path('profile/28/', views.profile),
 	path('profile/<int:username>/', views.profile),
 	path('profile/<slug:article_value>/', views.article),
 	path('profile/', views.profile)

50a43ff27fda37aec265d7e258d625ea035a7ce5
Rodrigo <rodrigocorrea2@hotmail.com>
2019-11-28 10:17:54 -0600

    date

--- todolist/todo/views.py
+++ todolist/todo/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
 from .models import Todo
 from .forms import TodoForm, NewTodoForm, UpdateTodoForm
+import datetime
 
 def index(request):
 	todo_list = Todo.objects.order_by('id')
 	# form = TodoForm()
 	newtodoform = NewTodoForm()
-	context = {'todo_list' : todo_list, 'form' : newtodoform}
+	mydate = datetime.datetime.now()
+	context = {'todo_list' : todo_list, 'form' : newtodoform, 'mydate' : mydate}
 	return render(request, 'todo/index.html', context)
 
 @require_POST
